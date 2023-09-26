000aeb6e6d31f280efab047d298851acb28798ed
duarte_3333 <dsmorais.33@gmail.com>
2023-09-26 19:43:55 +0100

    added OMP and lassoLARS regression

--- lab1.py
+++ lab1.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model, preprocessing
 from sklearn.model_selection import cross_validate
+from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
 from statistics import mean
 
@@ -61,8 +62,28 @@ def elastic_regression(X_train, y_train, k):
     print("elactic - Melhor lambda", elastic_lambda, "\n")
     return model, elastic_MSE
 
-def lasso_lars_regression(X_train, y_train, k):
-    lassoLars_reg = linear_model.LassoLarsCV(cv=k, normalize=False).fit(X, np.ravel(Y))
+def lasso_lars_regression(X_train, y_train, k): #bad idea: just for high dimensional, uncorrelated features
+    model = (linear_model.LassoLarsCV(cv=k))
+    model.fit(X_train, np.ravel(y_train))
+    lasso_lars_lambda = model.alpha_  # Best lambda
+    new_model = cross_validate(linear_model.LassoLars(alpha=lasso_lars_lambda), X_train, y_train, cv=k,
+                               scoring='neg_mean_squared_error', return_train_score=True)
+    lasso_lars_MSE = abs(new_model['test_score'].mean())
+    print("lasso_lars - Erro quadratico medio de teste", lasso_lars_MSE)
+    print("lasso_lars - Melhor lambda", lasso_lars_lambda, "\n")
+    return model, lasso_lars_MSE
+
+def orthogonal_matching_pursuit_regression(X_train, y_train, k): #bad idea: problem is not that overdeterminated
+    model = (linear_model.OrthogonalMatchingPursuitCV(cv=k, max_iter=10))
+    model.fit(X_train, np.ravel(y_train))
+    omp_n_zero_coef = model.n_nonzero_coefs_  # Choose number of non zero coefficients that best fits the data
+    new_model = cross_validate(
+        linear_model.OrthogonalMatchingPursuit(n_nonzero_coefs=omp_n_zero_coef), X_train, y_train, cv=k,
+        scoring='neg_mean_squared_error', return_train_score=True)
+    omp_MSE = abs(new_model['test_score'].mean())
+    print("omp - Erro quadratico medio de teste", omp_MSE)
+    print("omp - Coeficiente nulos", omp_n_zero_coef, "\n")
+    return model, omp_MSE
 
 def normalize_data(X_train, X_test):
     scaler = preprocessing.StandardScaler()
@@ -108,11 +129,12 @@ def predict(model, X_train, y_train, X_test):
 
 def main():
     X_train, y_train, X_test = load_data()
-    # corr_matrix(X_train) #See correlation between the features of the dataset
+    #corr_matrix(X_train) #See correlation between the features of the dataset
     X_train_norm, X_test_norm = normalize_data(X_train, X_test)  # Removing the mean and scaling to unit variance.
     k = 5  # Number splits, each with 15/5=3 elements(for k=5), test with k-fold cross validation
     functions = [linear_regression_model, ridge_regression,
-                 lasso_regression, elastic_regression]
+                 lasso_regression, lasso_lars_regression,
+                 orthogonal_matching_pursuit_regression]
     best_model = get_best_model(functions, X_train_norm, y_train, k)
     y_predict = predict(best_model, X_train, y_train, X_test)
     np.save('Y_Predicted.npy', y_predict)
