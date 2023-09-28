7b7c9f40b5846574d0a6f70f06c666354c87d25d
Dias <joaocnvdias@gmail.com>
2023-09-28 14:38:47 +0100

    Fixed elastic and predict

--- Pb1_LinearRegression/lab1.py
+++ Pb1_LinearRegression/lab1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model, preprocessing
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import cross_validate, cross_val_predict
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
 from statistics import mean
@@ -44,35 +44,37 @@ def lasso_regression(X_train, y_train, k):
     lasso_MSE = abs(cross_model['test_score'].mean())
     print("lasso - Erro quadratico medio de teste", lasso_MSE)
     print("lasso - Melhor lambda", lasso_lambda, "\n")
-    return model, lasso_MSE
+    return new_model, lasso_MSE
 
 
 def elastic_regression(X_train, y_train, k):
     elastic_ratios = create_lambdas(0.1, 0.9, 0.1)
-    lambda_ratios = create_lambdas(0.1, 0.9, 0.1)
-    model = linear_model.ElasticNetCV(alphas=lambda_ratios, l1_ratio=elastic_ratios, random_state=0, cv = k)
+    elastic_lambdas = create_lambdas(0.1, 10, 0.1)
+    model = linear_model.ElasticNetCV(alphas=elastic_lambdas, l1_ratio=elastic_ratios, random_state=0, cv = k)
     model.fit(X_train, np.ravel(y_train))
     elastic_lambda = model.alpha_
-    elastic_l1_ratio = model.l1_ratio
+    elastic_l1_ratio = model.l1_ratio_
     new_model = linear_model.ElasticNet(alpha = elastic_lambda, l1_ratio=elastic_l1_ratio)
     cross_model = cross_validate(new_model, X_train, y_train, cv=k,
                                  scoring='neg_mean_squared_error', return_train_score=True)
     elastic_MSE = abs(cross_model['test_score'].mean())
     print("elastic - Erro quadratico medio de teste", elastic_MSE)
+    print("elastic - Melhor racio entre lambdas", elastic_l1_ratio)
     print("elactic - Melhor lambda", elastic_lambda, "\n")
-    return model, elastic_MSE
+    
+    return new_model, elastic_MSE
 
 def lasso_lars_regression(X_train, y_train, k): #bad idea: just for high dimensional, uncorrelated features
     model = (linear_model.LassoLarsCV(cv=k))
     model.fit(X_train, np.ravel(y_train))
     lasso_lars_lambda = model.alpha_  # Best lambda
-    new_model = cross_validate(linear_model.LassoLars(alpha=lasso_lars_lambda), X_train, y_train, cv=k,
+    new_model = linear_model.LassoLars(alpha=lasso_lars_lambda)
+    cross_model = cross_validate(new_model, X_train, y_train, cv=k,
                                scoring='neg_mean_squared_error', return_train_score=True)
-    lasso_lars_MSE = abs(new_model['test_score'].mean())
+    lasso_lars_MSE = abs(cross_model['test_score'].mean())
     print("lasso_lars - Erro quadratico medio de teste", lasso_lars_MSE)
     print("lasso_lars - Melhor lambda", lasso_lars_lambda, "\n")
-    return model, lasso_lars_MSE
-
+    return new_model, lasso_lars_MSE
 
 def bayesian_regression(X_train, y_train, k):
     model = linear_model.ARDRegression()
@@ -81,19 +83,21 @@ def bayesian_regression(X_train, y_train, k):
                                 return_train_score=True)
     bayesian_mse = abs(cv_results['test_score'].mean())
     print("bayesian -  Erro quadratico medio:", bayesian_mse, "\n")
-    return model, bayesian_mse
+    return cv_results, bayesian_mse
 
 def orthogonal_matching_pursuit_regression(X_train, y_train, k): #good idea: problem is overdeterminated
     model = (linear_model.OrthogonalMatchingPursuitCV(cv=k, max_iter=10))
     model.fit(X_train, np.ravel(y_train))
     omp_n_zero_coef = model.n_nonzero_coefs_  # Choose number of non zero coefficients that best fits the data
-    new_model = cross_validate(
-        linear_model.OrthogonalMatchingPursuit(n_nonzero_coefs=omp_n_zero_coef), X_train, y_train, cv=k,
+    new_model = linear_model.OrthogonalMatchingPursuit(n_nonzero_coefs=omp_n_zero_coef)
+    cross_model = cross_validate(new_model 
+        , X_train, y_train, cv=k,
         scoring='neg_mean_squared_error', return_train_score=True)
-    omp_MSE = abs(new_model['test_score'].mean())
+    omp_MSE = abs(cross_model['test_score'].mean())
     print("omp - Erro quadratico medio de teste", omp_MSE)
     print("omp - Coeficiente nulos", omp_n_zero_coef, "\n")
-    return model, omp_MSE
+    
+    return new_model, omp_MSE
 
 def normalize_data(X_train, X_test):
     scaler = preprocessing.StandardScaler()
@@ -118,6 +122,13 @@ def corr_matrix(X_train):
     plt.yticks(range(correlation_matrix.shape[0]))
     plt.show()
 
+def histogram(X_train):
+    plt.hist(X_train, bins=7)
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of training data "X"')
+    plt.show()
+
 def get_best_model(functions, X, y, k):
     final_model = None
     final_error = float('inf')
@@ -126,29 +137,33 @@ def get_best_model(functions, X, y, k):
         if error < final_error:
             final_error = error
             final_model = model
+            
     print("final model", final_model)
     print("final error", final_error)
+    
     return final_model
 
-def predict(model, X_train, y_train, X_test):
+def predict(model, X_train, y_train, X_test): 
     model.fit(X_train, y_train)
     y_predict = model.predict(X_test)
+    
     # print(model.coef_)
     # print(model.intercept_)
     return y_predict
 
 def main():
     X_train, y_train, X_test = load_data()
-    #corr_matrix(X_train) #See correlation between the features of the dataset
     X_train_norm, X_test_norm = normalize_data(X_train, X_test)  # Removing the mean and scaling to unit variance.
+    # corr_matrix(X_train) #See correlation between the features of the dataset
     k = 10  # Number splits, each with 15/5=3 elements(for k=5), test with k-fold cross validation
     functions = [linear_regression_model, ridge_regression,
-                 lasso_regression, lasso_lars_regression,
-                 bayesian_regression,
-                 orthogonal_matching_pursuit_regression]
+                 lasso_regression, lasso_lars_regression, 
+                 elastic_regression, orthogonal_matching_pursuit_regression]
     best_model = get_best_model(functions, X_train, y_train, k)
     y_predict = predict(best_model, X_train, y_train, X_test)
+    print(y_predict)
     np.save('Y_Predicted.npy', y_predict)
 
-
-main()
+  
+if __name__ == '__main__':
+    main()
