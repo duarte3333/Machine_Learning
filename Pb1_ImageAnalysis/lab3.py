f693b677b9fdc79ad819352001c3d31654753209
duarte_3333 <dsmorais.33@gmail.com>
2023-10-17 17:31:40 +0100

    added early stopping

--- Pb1_ImageAnalysis/lab3.py
+++ Pb1_ImageAnalysis/lab3.py
@@ -56,9 +56,11 @@ def show_images(X_train, y_train):
 def main():
     X_train, y_train, X_test = load_data()
     show_images(X_train, y_train)
-    #X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+    MLP(X_train, y_train, parameters)
     #X_train, X_test = standardization(X_train, X_test)
     
 
 if __name__ == '__main__':
     main()
+    1
